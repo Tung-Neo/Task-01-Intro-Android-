3c0851dddfd0b58eba9deb0e9228d05223425c8f
Tung-Neo <62163195+Tung-Neo@users.noreply.github.com>
2021-08-12 08:07:33 +0700

    Task 1

---
+++ SayHello/app/src/main/java/com/example/sayheloo/SayHello.java
@@ -0,0 +1,32 @@
+package com.example.sayheloo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SayHello extends AppCompatActivity{
+    private Button myButton;
+    private TextView showText;
+    private EditText enterName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myButton = findViewById(R.id.btnSay);
+        enterName = findViewById(R.id.txtName);
+        showText = findViewById(R.id.tvwHello);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = enterName.getText().toString();
+                showText.setText("Hello " + name);
+            }
+        });
+    }
+}
